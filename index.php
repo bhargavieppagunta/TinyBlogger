04129384a3fc9707d96eb0f0a4922b41b9b46b4b
bhargavieppagunta <eppaguntabhargavi031@gmail.com>
2024-03-07 16:25:48 -0600

    Index Page and Styling

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Custume Styling -->
+    <link rel="stylesheet" href="assets/css/style.css"> 
+    <title>Blog</title>
+</head>
+<body>
+    <!-- Page Wrapper -->
+    <div class="page-wrapper">
+        <!--Content-->
+        <div class="content clearfix">
+            <!-- Main Content -->
+            <div class="main-content">
+            <h1 class="recent-post-title">Recent Posts</h1>
+                    <div class="post clearfix">
+                            <h2><a href="#">iPhone 15, iPhone 15 Pro launched with new camera, titanium frame</a></h2>
+                            <i class="far fa-user">&nbsp;Bhargavi</i>
+                            &nbsp;
+                            <i class="far fa-calendar">&nbsp;March, 4th 2024</i>
+                            <p class="preview-text">
+                            After months of leaks, Apple has finally announced the new iPhone 15 series. This year too, the company has introduced four models
+                            </p>
+                            <a href="#" class="btn read-more">Read More</a>
+                        </div>
+                    </div>
+                    <div class="post clearfix">
+                        
+                        <div class="post-preview">
+                            <h2><a href="#">Ind vs Eng: Jasprit Bumrah Rested for the Fourth Test</a></h2>
+                            <i class="far fa-user">&nbsp;Bhargavi</i>
+                            &nbsp;
+                            <i class="far fa-calendar">&nbsp;March, 4th 2024</i>
+                            <p class="preview-text">
+                            Indian pacer Jasprit Bumrah has been given rest for the fourth Test between India and England set to take place in Ranchi
+                            </p>
+                            <a href="#" class="btn read-more">Read More</a>
+                        </div>
+                    </div>
+                    <div class="post clearfix"> 
+                        <div class="post-preview">
+                            <h2><a href="#">First Gen Z Indian-American to Contest US Senate Race</a></h2>
+                            <i class="far fa-user">&nbsp;Bhargavi</i>
+                            &nbsp;
+                            <i class="far fa-calendar">&nbsp;March, 4th 2024</i>
+                            <p class="preview-text">
+                            Twenty-four-year-old Indian-American software engineer Ashwin Ramaswami is all set to enter the American political arena.
+                            </p>
+                            <a href="#" class="btn read-more">Read More</a>
+                        </div>
+                    </div>
+                    <div class="post clearfix">                       
+                        <div class="post-preview">
+                            <h2><a href="#">Ind vs Eng: Jasprit Bumrah Rested for the Fourth Test</a></h2>
+                            <i class="far fa-user">&nbsp;Bhargavi</i>
+                            &nbsp;
+                            <i class="far fa-calendar">&nbsp;March, 4th 2024</i>
+                            <p class="preview-text">
+                                
+                            </p>
+                            <a href="#" class="btn read-more">Read More</a>
+                        </div>
+                    </div>
+                    <div class="post clearfix">
+                        <div class="post-preview">
+                            <h2><a href="#">First Gen Z Indian-American to Contest US Senate Race</a></h2>
+                            <i class="far fa-user">&nbsp;Bhargavi</i>
+                            &nbsp;
+                            <i class="far fa-calendar">&nbsp;March, 4th 2024</i>
+                            <p class="preview-text">
+                            Twenty-four-year-old Indian-American software engineer Ashwin Ramaswami is all set to enter the American political arena.
+                            </p>
+                            <a href="#" class="btn read-more">Read More</a>
+                        </div>
+                    </div>
+                    <div class="post clearfix">
+                        <div class="post-preview">
+                            <h2><a href="#">Ind vs Eng: Jasprit Bumrah Rested for the Fourth Test</a></h2>
+                            <i class="far fa-user">&nbsp;Bhargavi</i>
+                            &nbsp;
+                            <i class="far fa-calendar">&nbsp;March, 4th 2024</i>
+                            <p class="preview-text">
+                                
+                            </p>
+                            <a href="#" class="btn read-more">Read More</a>
+                        </div>
+                    </div>
+            </div>
+        </div>
+        <!-- //Content -->
+    </div>
+    <!-- //Page Wrapper -->
+</body>
+</html>
