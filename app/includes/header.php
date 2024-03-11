77c93762b747f9741fa2ab6ccc179cde0edcecea
nithinsiddhartha <161191373+nithinsiddhartha@users.noreply.github.com>
2024-03-11 15:01:21 -0500

    Adding a header section to our webpage.

---
+++ app/includes/header.php
@@ -0,0 +1,22 @@
+<header>
+    <a href="" class="logo">
+        <h2 class="logo-text"><span>Tiny Blogger</span></h2>
+    </a>
+    <i class="fa fa-bars menu-toggle"></i>
+    <ul class="nav">
+        <li>
+            <a href="">Home</a>
+        </li>
+        <li>
+            <a href="">About</a>
+        </li>
+        <li>
+            <ul>
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#" class="logout">Logout</a></li> 
+            </ul>
+        </li>
+            <li><a href="">Sign Up</a></li>
+            <li><a href="">Login</a></li>
+    </ul>
+</header>
