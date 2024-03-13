14804c40865a1968f94e2f40ad2d1377555cff53
bhargavieppagunta <eppaguntabhargavi031@gmail.com>
2024-03-12 20:38:47 -0500

    Designing a Single blog post
    This page contains the content of particular post. For now it's just the outline for a single page.

--- index.php
+++ index.php
@@ -19,79 +19,79 @@
                     <div class="post clearfix">
                         <img src="" class="post-image" alt="">
                         <div class="post-preview">
-                            <h2><a href="#">iPhone 15, iPhone 15 Pro launched with new camera, titanium frame</a></h2>
+                            <h2><a href="single.php">iPhone 15, iPhone 15 Pro launched with new camera, titanium frame</a></h2>
                             <i class="far fa-user">&nbsp;Bhargavi</i>
                             &nbsp;
                             <i class="far fa-calendar">&nbsp;March, 4th 2024</i>
                             <p class="preview-text">
                             After months of leaks, Apple has finally announced the new iPhone 15 series. This year too, the company has introduced four models
                             </p>
-                            <a href="#" class="btn read-more">Read More</a>
+                            <a href="single.php" class="btn read-more">Read More</a>
                         </div>
                     </div>
                     <div class="post clearfix">
                         <img src="" class="post-image" alt="">
                         <div class="post-preview">
-                            <h2><a href="#">Ind vs Eng: Jasprit Bumrah Rested for the Fourth Test</a></h2>
+                            <h2><a href="single.php">Ind vs Eng: Jasprit Bumrah Rested for the Fourth Test</a></h2>
                             <i class="far fa-user">&nbsp;Bhargavi</i>
                             &nbsp;
                             <i class="far fa-calendar">&nbsp;March, 4th 2024</i>
                             <p class="preview-text">
                             Indian pacer Jasprit Bumrah has been given rest for the fourth Test between India and England set to take place in Ranchi
                             </p>
-                            <a href="#" class="btn read-more">Read More</a>
+                            <a href="single.php" class="btn read-more">Read More</a>
                         </div>
                     </div>
                     <div class="post clearfix">
                         <img src="" class="post-image" alt="">
                         <div class="post-preview">
-                            <h2><a href="#">First Gen Z Indian-American to Contest US Senate Race</a></h2>
+                            <h2><a href="single.php">First Gen Z Indian-American to Contest US Senate Race</a></h2>
                             <i class="far fa-user">&nbsp;Bhargavi</i>
                             &nbsp;
                             <i class="far fa-calendar">&nbsp;March, 4th 2024</i>
                             <p class="preview-text">
                             Twenty-four-year-old Indian-American software engineer Ashwin Ramaswami is all set to enter the American political arena.
                             </p>
-                            <a href="#" class="btn read-more">Read More</a>
+                            <a href="single.php" class="btn read-more">Read More</a>
                         </div>
                     </div>
                     <div class="post clearfix">
                         <img src="" class="post-image" alt="">
                         <div class="post-preview">
-                            <h2><a href="#">Ind vs Eng: Jasprit Bumrah Rested for the Fourth Test</a></h2>
+                            <h2><a href="single.php">Ind vs Eng: Jasprit Bumrah Rested for the Fourth Test</a></h2>
                             <i class="far fa-user">&nbsp;Bhargavi</i>
                             &nbsp;
                             <i class="far fa-calendar">&nbsp;March, 4th 2024</i>
                             <p class="preview-text">
                                 
                             </p>
-                            <a href="#" class="btn read-more">Read More</a>
+                            <a href="single.php" class="btn read-more">Read More</a>
                         </div>
                     </div>
                     <div class="post clearfix">
                         <img src="" class="post-image" alt="">
                         <div class="post-preview">
-                            <h2><a href="#">First Gen Z Indian-American to Contest US Senate Race</a></h2>
+                            <h2><a href="single.php">First Gen Z Indian-American to Contest US Senate Race</a></h2>
                             <i class="far fa-user">&nbsp;Bhargavi</i>
                             &nbsp;
                             <i class="far fa-calendar">&nbsp;March, 4th 2024</i>
                             <p class="preview-text">
                             Twenty-four-year-old Indian-American software engineer Ashwin Ramaswami is all set to enter the American political arena.
                             </p>
-                            <a href="#" class="btn read-more">Read More</a>
+                            <a href="single.php" class="btn read-more">Read More</a>
                         </div>
                     </div>
                     <div class="post clearfix">
                         <img src="" class="post-image" alt="">
                         <div class="post-preview">
-                            <h2><a href="#">Ind vs Eng: Jasprit Bumrah Rested for the Fourth Test</a></h2>
+                            <h2><a href="single.php">Ind vs Eng: Jasprit Bumrah Rested for the Fourth Test</a></h2>
                             <i class="far fa-user">&nbsp;Bhargavi</i>
                             &nbsp;
                             <i class="far fa-calendar">&nbsp;March, 4th 2024</i>
                             <p class="preview-text">
                                 
                             </p>
-                            <a href="#" class="btn read-more">Read More</a>
+                            <a href="single.php" class="btn read-more">Read More</a>
                         </div>
                     </div>
             </div>
