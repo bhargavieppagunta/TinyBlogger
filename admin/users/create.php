f72931e613357294c9648fc8418fe20cbcf6d7d6
PranathiBRaju <pranathiraju11@gmail.com>
2024-03-29 23:23:05 -0500

    Developing-Create- and-Edit-Page-for-Users
    Developing a user-friendly interface for creating and editing user profiles.

---
+++ admin/users/create.php
@@ -0,0 +1,67 @@
+<?php include("../../path.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/admin.css">
+    
+    <title>Admin Section - Add User</title>
+</head>
+<body>
+    
+<?php include(ROOT_PATH . "/app/includes/adminHeader.php"); ?>
+
+    <!-- Admin Page Wrapper -->
+    <div class="admin-wrapper">
+        
+    <?php include(ROOT_PATH . "/app/includes/adminSidebar.php"); ?>
+
+        <!-- // Left Sidebar-->
+        
+        <!-- Admin Content-->
+        <div class="admin-content">
+            <div class="button-group">
+                <a href="create.php" class="btn btn-big">Add User</a>
+                <a href="index.php" class="btn btn-big">Manage Users</a>
+            </div>
+            <div class="content">
+                <h2 class="page-title">Add User</h2>
+
+                <form action="create.php" method="post">
+                    <div>
+                        <label>Username</label>
+                        <input type="text" name="username" value="" class="text-input">
+                    </div>
+                    <div>
+                        <label>Email</label>
+                        <input type="email" name="email" value="" class="text-input">
+                    </div>
+                    <div>
+                        <label>Password</label>
+                        <input type="password" name="password" value="" class="text-input">
+                    </div>
+                    <div>
+                        <label>Password Confirmation</label>
+                        <input type="password" name="passwordConf" value="" class="text-input">
+                    </div>
+                    <div>
+                            <label>
+                                <input type="checkbox" name="admin" checked>
+                                Admin
+                            </label>
+                    </div>
+                    <div>
+                        <button type="submit" name="create-admin" class=" btn btn-big"><span>Add User</span></button>
+                    </div>
+
+                </form>
+
+            </div>
+        </div>
+        <!-- // Admin Content-->
+    </div>
+    <!-- //Page Wrapper -->
+</body>
+</html>
