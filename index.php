45bda4ad140ca4cb0422475ddb031354b1b1654e
sahithipadigela <sahithipadigela3011@gmail.com>
2024-04-16 16:18:20 -0500

    Adding a load more button to index page

--- index.php
+++ index.php
@@ -31,27 +31,35 @@ if (isset($_GET['t_id'])) {
         <div class="content clearfix">
             <!-- Main Content -->
             <div class="main-content">
-                <h1 class="recent-post-title"><?php echo $postsTitle ?> </h1>
-                <?php 
-                $i = 0;
-                foreach ($posts as $post) : ?>
-                
-                <?php if ($i++ > 3) break; ?>
-                    <div class="post clearfix">
-                        <img src="<?php echo BASE_URL . '/assets/images/' . $post['image']; ?>" class="post-image" alt="">
-                        <div class="post-preview">
-                            <h2><a href="single.php?id=<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a></h2>
-                            <i class="far fa-user">&nbsp;<?php echo $post['username']; ?></i>
-                            &nbsp;
-                            <i class="far fa-calendar">&nbsp;<?php echo date('F j, Y', strtotime($post['created_at'])); ?></i>
-                            <p class="preview-text">
-                                <?php echo html_entity_decode(substr($post['body'], 0, 120) . '...'); ?>
-                            </p>
-                            <a href="single.php?id=<?php echo $post['id']; ?>" class="btn read-more">Read More</a>
-                        </div>
+            <h1 class="recent-post-title"><?php echo $postsTitle ?> </h1>
+    <div id="posts-container">
+        <?php
+        $i = 0;
+        foreach ($posts as $post) :
+            if ($i++ < 4) :
+        ?>
+                <div class="post clearfix">
+                    <img src="<?php echo BASE_URL . '/assets/images/' . $post['image']; ?>" class="post-image" alt="">
+                    <div class="post-preview">
+                        <h2><a href="single.php?id=<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a></h2>
+                        <i class="far fa-user">&nbsp;<?php echo $post['username']; ?></i>
+                        &nbsp;
+                        <i class="far fa-calendar">&nbsp;<?php echo date('F j, Y', strtotime($post['created_at'])); ?></i>
+                        <p class="preview-text">
+                            <?php echo html_entity_decode(substr($post['body'], 0, 120) . '...'); ?>
+                        </p>
+                        <a href="single.php?id=<?php echo $post['id']; ?>" class="btn read-more">Read More</a>
                     </div>
-                <?php endforeach; ?>
-            </div>
+                </div>
+        <?php
+            endif;
+        endforeach;
+        ?>
+        <?php if (count($posts) > 4) : ?>
+        <button id="loadMoreBtn" class="btn load-more">Load More</button>
+    <?php endif; ?>
+    </div>
+</div>
             <!-- //Main Content -->
             <div class="sidebar">
                
@@ -76,5 +84,37 @@ if (isset($_GET['t_id'])) {
     </div>
     <!-- //Page Wrapper -->
 <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
+<script>
+    var remainingPosts = <?php echo json_encode(array_slice($posts, 4)); ?>;
+    var loadMoreBtn = document.getElementById('loadMoreBtn');
+    var postContainer = document.getElementById('posts-container');
+
+    loadMoreBtn.addEventListener('click', function() {
+        var nextThreePosts = remainingPosts.slice(0, 3);
+        remainingPosts = remainingPosts.slice(3);
+
+        nextThreePosts.forEach(function(post) {
+            var postDiv = document.createElement('div');
+            postDiv.classList.add('post', 'clearfix');
+            postDiv.innerHTML = `
+                    <img src="<?php echo BASE_URL . '/assets/images/' . $post['image']; ?>" class="post-image" alt="">
+                    <div class="post-preview">
+                        <h2><a href="single.php?id=<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a></h2>
+                        <i class="far fa-user">&nbsp;<?php echo $post['username']; ?></i>
+                        &nbsp;
+                        <i class="far fa-calendar">&nbsp;<?php echo date('F j, Y', strtotime($post['created_at'])); ?></i>
+                        <p class="preview-text">
+                            <?php echo html_entity_decode(substr($post['body'], 0, 120) . '...'); ?>
+                        </p>
+                        <a href="single.php?id=<?php echo $post['id']; ?>" class="btn read-more">Read More</a>
+                    </div>`;
+            postContainer.appendChild(postDiv);
+        });
+
+        if (remainingPosts.length === 0) {
+            loadMoreBtn.style.display = 'none';
+        }
+    });
+</script>
 </body>
 </html>
